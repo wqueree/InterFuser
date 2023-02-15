9222af1546c88d9cfa1761ef9bc4d6e723e53a7a
Will Queree <wqueree@gmail.com>
2023-02-15 09:19:17 +0000

    Scripting

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -1,2 +1,1 @@
-# hare run -it -v /homes/wlq20/CM30082/InterFuser:/InterFuser --gpus '"device=3,5"' wlq20/interfuser
 hare run -it --gpus '"device=3,5"' wlq20/interfuser
---
+++ init.sh
@@ -0,0 +1,3 @@
+git pull
+conda activate interfuser
+cd InterFuser
